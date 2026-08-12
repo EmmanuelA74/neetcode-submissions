fbc8275565ca899de5f231b61f53820a1c29cd3b
EmmanuelA74 <76852056+EmmanuelA74@users.noreply.github.com>
2026-08-12 12:55:19 -0400

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def backtrack(curr, start):
+            #we're at the end of the string
+            if start == len(s):
+                ans.append(curr[:])
+                return 
+            
+            for i in range(start, len(s)):
+                substring = s[start : i + 1]
+
+                if substring == substring[::-1]:
+                    curr.append(substring)
+                    backtrack(curr, i + 1)
+                    curr.pop()
+                
+                    
+        ans = []
+        backtrack([], 0)
+
+        return ans 
